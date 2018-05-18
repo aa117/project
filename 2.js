2338fd6badf2da13641ba9eb2f71ec9ee4de5fc0
aa117 <1013890926@qq.com>
2018-05-18 21:42:36 +0800

    第一次提交

---
+++ 2.js
@@ -0,0 +1,21 @@
+//e f交换
+var e=10;
+var f=20;
+var g=f;
+g=e;
+e=f;
+f=g;
+console.log(e, f);
+//a b交换
+var a=10;
+var b=20;
+a=a+b;//a 30
+b=a-b;//b 10
+a=a-b;//a 20
+console.log(a, b);
+var c=10;
+var d=20;
+[c,d]=[d,c];
+console.log(c, d);
+//aaaa
+
